7bb80f300b83980be3e8f05299a5fff3920d5d2d
BuildTools <unconfigured@null.spigotmc.org>
2023-06-04 01:41:07 +0200

    Dodaj pliki projektów.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace RandomNumberGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Random Number Generator");
+
+            Console.Write("Enter the minimum number: ");
+            int min = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the maximum number: ");
+            int max = Convert.ToInt32(Console.ReadLine());
+
+            Random rnd = new Random();
+            int randomNumber = rnd.Next(min, max + 1);
+
+            Console.WriteLine($"Random number between {min} and {max}: {randomNumber}");
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
